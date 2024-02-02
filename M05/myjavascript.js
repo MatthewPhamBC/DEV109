583a04b2cf1f53e8f9ee885972cae3f6056a3d79
MatthewPhamBC <155837401+MatthewPhamBC@users.noreply.github.com>
2024-02-01 18:14:12 -0800

    Update myjavascript.js

--- M05/myjavascript.js
+++ M05/myjavascript.js
@@ -1,9 +1,9 @@
-var chooseNum = prompt("Please enter a number between 0-10.");
-var math = parseInt(chooseNum);
-var table = math;             // Unit of table
+var chooseNum = prompt("Please enter a number between 0-10."); // Ask user to enter value.
+var math = parseInt(chooseNum); 
+var table = math; // Unit of table
 var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '<h2>Multiplication Table</h2>';              // Message
+var i = 1; // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>'; // Message
 
 if (operator === 'addition') {
   // Do addition
@@ -11,13 +11,14 @@ if (operator === 'addition') {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
-} else {
+} /* else {
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
 }
+*/
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
