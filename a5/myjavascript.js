275348d31b02b6bcddfd1aaf928d2a765bab36ba
MatthewPhamBC <155837401+MatthewPhamBC@users.noreply.github.com>
2024-03-09 22:00:40 -0800

    Update myjavascript.js

--- a5/myjavascript.js
+++ a5/myjavascript.js
@@ -1,14 +1,37 @@
 i = 0;
-myImages = ["https://static.vecteezy.com/system/resources/thumbnails/020/336/375/small/nike-logo-nike-icon-free-free-vector.jpg", 
-"https://www.shutterstock.com/image-vector/valencia-spain-april-25-2023-600nw-2293213071.jpg", "https://static.vecteezy.com/system/resources/thumbnails/010/994/431/small_2x/puma-logo-black-symbol-with-name-clothes-design-icon-abstract-football-illustration-with-white-background-free-vector.jpg", 
-"https://seeklogo.com/images/N/New_Balance-logo-F34722CB97-seeklogo.com.png", "https://upload.wikimedia.org/wikipedia/commons/5/53/Reebok_2019_logo.svg"];
-myPhrases = ["Nike", "Adidas", "Puma", "NewBalance", "Reebok"];
-window.setInterval(showPhrases, 2000);
+myImages = ["https://images.pexels.com/photos/6462662/pexels-photo-6462662.png?cs=srgb&dl=pexels-broderick-armbrister-6462662.jpg&fm=jpg", 
+"https://cdn.pixabay.com/photo/2023/05/05/04/38/ai-generated-7971480_1280.png", "https://pixexid.com/api/download/image/sports-car-concept-for-lexus-lfa-ofhh6sg7.jpeg", 
+"https://media.sketchfab.com/models/ff8fb2251dfa4bb9979e7022c5a6666c/thumbnails/5d94951eea044d9b92596e176378a968/f8a32c8c1ea440799bfc1c22dae22369.jpeg", "https://images1.the-dots.com/3420977/poast-this-one-only-the-best.png?p=projectImageFullJpg"];
+myPhrases = ["lamborghini ", "bugatti", "Lexus", "Nissan", "Mclaren"];
+window.setInterval(repeat, 6000);
+
+function repeat(){
+    window.setInterval(showPhrases, 5000, i++);
+}
+
+var prevButton = document.getElementById("prev");
+prevButton.addEventListener("click", function() {
+    showPhrases(i--)
+});
+
+var nextButton = document.getElementById("next");
+nextButton.addEventListener("click", function() {
+        showPhrases(i++);
+});
 
 function showPhrases(){
-        if (i < myPhrases.length)  
-        { document.getElementById("Phrases").innerHTML = "<h1>" + myPhrases[i] + "</h1>";
-        i++;}
-        else 
-        i = 0;
-}
+    if(i < myPhrases.length)  
+    { 
+    if(i < 0){
+        i = 0
+    } 
+    else {
+        document.getElementById("Phrases").innerHTML = "<h1>" + myPhrases[i] + "</h1>";
+        document.getElementById("newImages").src=myImages[i];
+    }
+        
+    }
+    else {
+    i = 0;
+    }
+} 
